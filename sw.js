19a9bc4e7026088f81e0716d0a53f069d20941fd
yuuuh26 <dengana.10011212@gmail.com>
2026-09-18 10:58:53 -0700

    Bump service worker cache for v1.1.0

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='part6-beat-v1.0.0';
+const CACHE='part6-beat-v1.1.0';
 const ASSETS=['./','./index.html','./styles.css','./data.js','./app.js','./manifest.webmanifest','./assets/icon-192.png','./assets/icon-512.png','./assets/icon-maskable-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));

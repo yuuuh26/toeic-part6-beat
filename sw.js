7d27b75423d9928574d476e1f1f7cb7c9a300d57
yuuuh26 <dengana.10011212@gmail.com>
2026-09-18 18:59:17 -0700

    Cache BGM controls and bump app cache to v1.2.0

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='part6-beat-v1.1.0';
-const ASSETS=['./','./index.html','./styles.css','./data.js','./app.js','./manifest.webmanifest','./assets/icon-192.png','./assets/icon-512.png','./assets/icon-maskable-512.png'];
+const CACHE='part6-beat-v1.2.0';
+const ASSETS=['./','./index.html','./styles.css','./bgm.css','./data.js','./app.js','./bgm.js','./manifest.webmanifest','./assets/icon-192.png','./assets/icon-512.png','./assets/icon-maskable-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match('./index.html'))))});
